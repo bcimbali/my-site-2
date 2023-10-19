897b98d953b6171c265bfd3ba51258b5fb206528
Brett Cimbalik <bcimbali@gmail.com>
2023-10-18 19:35:08 -0500

    feat(setup): add practical css "reset"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import styles from './page.module.css';
 import HomePage from '@/components/HomePage';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <HomePage />
     </main>
   );
