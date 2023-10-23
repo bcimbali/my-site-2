c11d2331f1f6e00e4b22387cde73381a6275d1e2
Brett Cimbalik <bcimbali@gmail.com>
2023-10-23 08:37:09 -0500

    fix(theme): add styledComponents to nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+      displayName: true,
+      // Enabled by default.
+      ssr: true,
+      // Enabled by default.
+      cssProp: true,
+      // Empty by default.
+      minify: true,
+      // Not supported yet.
+      transpileTemplateLiterals: true
+      // Not supported yet.
+      // pure?: boolean,
+    }
+  }
+};
 
 module.exports = nextConfig;
