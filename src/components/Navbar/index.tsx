6decbc5e27caa1c375de4a8c6a1c401869cf3a44
Brett Cimbalik <bcimbali@gmail.com>
2023-10-24 20:17:57 -0500

    feat(component): add desktop nav item links

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,51 @@
 import styled, { css } from 'styled-components';
 import Link from 'next/link';
 
-const Logo = styled.h2``;
+const DesktopLink = styled(Link)`
+  ${({ theme: { colors } }) => css`
+    color: ${colors.white};
+    display: flex;
+    gap: 0.25rem;
+  `}
+`;
+
+const DesktopLinkContainer = styled.ul`
+  ${({ theme: { mq } }) => css`
+    align-items: center;
+    display: none;
+    gap: 1.5rem;
+    list-style-type: none;
+    padding: 0;
+
+    ${mq('lg')`
+      display: flex;
+    `}
+  `}
+`;
+
+const DesktopLinkItem = styled.li`
+  display: inline-flex;
+  list-style-type: none;
+`;
+
+const DesktopLinkNumber = styled.span`
+  align-items: flex-end;
+  display: inline-flex;
+  font-size: 0.5rem;
+`;
+
+const DesktopLinkTitle = styled.h4``;
 
 const InnerNav = styled.div`
   ${({ theme: { maxWidth } }) => css`
+    display: flex;
+    justify-content: space-between;
     max-width: ${maxWidth};
     width: 100%;
   `}
 `;
+
+const Logo = styled.h2``;
 
 const OuterNav = styled.nav`
   ${({ theme: { colors, layout, mq, nav } }) => css`
@@ -43,6 +80,28 @@ const Navbar = () => {
         <StyledLink href="/">
           <Logo>Brett Cimbalik</Logo>
         </StyledLink>
+        <DesktopLinkContainer>
+          <DesktopLinkItem>
+            <DesktopLink href="/projects">
+              <DesktopLinkNumber>01</DesktopLinkNumber>
+              <DesktopLinkTitle>Projects</DesktopLinkTitle>
+            </DesktopLink>
+          </DesktopLinkItem>
+
+          <DesktopLinkItem>
+            <DesktopLink href="/projects">
+              <DesktopLinkNumber>02</DesktopLinkNumber>
+              <DesktopLinkTitle>About</DesktopLinkTitle>
+            </DesktopLink>
+          </DesktopLinkItem>
+
+          <DesktopLinkItem>
+            <DesktopLink href="/projects">
+              <DesktopLinkNumber>03</DesktopLinkNumber>
+              <DesktopLinkTitle>Contact</DesktopLinkTitle>
+            </DesktopLink>
+          </DesktopLinkItem>
+        </DesktopLinkContainer>
       </InnerNav>
     </OuterNav>
   );
