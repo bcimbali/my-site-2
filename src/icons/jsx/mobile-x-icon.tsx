1c7ea4f82affd76ae63ac81d652f395d4f9ace21
Brett Cimbalik <bcimbali@gmail.com>
2023-10-26 08:54:16 -0500

    feat(build): update svgr script to create jsx

--- src/icons/jsx/mobile-x-icon.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import type { SVGProps } from 'react';
-const SvgMobileXIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={32} height={16} fill="none" {...props}>
-    <path stroke="#fff" strokeWidth={2} d="m31.588 14.911-31-14M31.423.906l-30 14" />
-  </svg>
-);
-export default SvgMobileXIcon;
-
