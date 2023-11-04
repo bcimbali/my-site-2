1cdf7045e282454b1f6a5915b7beef55e3eadfd0
Brett Cimbalik <bcimbali@gmail.com>
2023-11-04 12:10:45 -0500

    feat(asset): add remaining project images

--- src/lib/projectsData.ts
+++ src/lib/projectsData.ts
@@ -11,7 +11,7 @@ const projectsData = [
   {
     title: 'ck-site',
     description: 'A fully customized portfolio for a professional copywriter.',
-    image: '/project-images/bc-010-1200x800.jpg',
+    image: '/project-images/ck-site-1200x800.jpg',
     codeLink: 'https://github.com/bcimbali/ck-site',
     liveLink: 'https://kanownik.co/',
     techStack: ['React', 'Gatsby.js', 'Styled Components']
@@ -19,7 +19,7 @@ const projectsData = [
   {
     title: 'sp-next-site',
     description: 'A band website with a custom audio player.',
-    image: '/project-images/bc-010-1200x800.jpg',
+    image: '/project-images/sp-next-site-1200x800.jpg',
     codeLink: 'https://github.com/bcimbali/sp-next-site',
     liveLink: 'https://sp-next-site.vercel.app/',
     techStack: ['React', 'Next.js', 'Styled Components', 'Amplitude.js']
