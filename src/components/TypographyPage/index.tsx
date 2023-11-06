930e813f32cafa76a97ae15a6218de47acfdf478
Brett Cimbalik <bcimbali@gmail.com>
2023-11-06 08:54:04 -0600

    wip(theme): add typography page for text updates

---
+++ src/components/TypographyPage/index.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import styled, { css } from 'styled-components';
+import React from 'react';
+
+const StyledDiv = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  grid-column: 1/-1;
+  justify-content: center;
+`;
+
+const PageTitle = styled.h1`
+  ${({ theme: { themeColors } }) => css`
+    color: ${themeColors.fg};
+    margin-bottom: 4rem;
+  `}
+`;
+
+const TypographyContainer = styled.div`
+  border: 1px dotted white;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+
+  * {
+    border: 1px dotted white;
+  }
+`;
+
+const TypographyPage = () => {
+  return (
+    <StyledDiv>
+      <PageTitle>Typography</PageTitle>
+      <TypographyContainer>
+        <h1>This is an h1 tag.</h1>
+        <h2>This is an h2 tag.</h2>
+        <h3>This is an h3 tag.</h3>
+        <h4>This is an h4 tag.</h4>
+        <h5>This is an h5 tag.</h5>
+        <h6>This is an h6 tag.</h6>
+        <p>This is a p tag.</p>
+      </TypographyContainer>
+    </StyledDiv>
+  );
+};
+
+export default TypographyPage;
