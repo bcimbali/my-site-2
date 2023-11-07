678d8398dc34b31e261a4affbcaabeea83b40a40
Brett Cimbalik <bcimbali@gmail.com>
2023-11-06 20:24:00 -0600

    feat(component): tweaks to the ProjectCards

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -12,34 +12,35 @@ const CardContainer = styled.li`
     display: flex;
     flex-direction: column;
     max-width: 27.5rem;
+    overflow: hidden;
   `}
 `;
 
-const ChipsContainer = styled.ul`
-  ${({ theme: { mediaQuery } }) => css`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 0.5rem;
-    justify-content: center;
-    list-style-type: none;
-    padding: 0;
+// const ChipsContainer = styled.ul`
+//   ${({ theme: { mediaQuery } }) => css`
+//     display: flex;
+//     flex-wrap: wrap;
+//     gap: 0.5rem;
+//     justify-content: center;
+//     list-style-type: none;
+//     padding: 0;
 
-    ${mediaQuery('md')`
-      justify-content: unset
-    `}
-  `}
-`;
+//     ${mediaQuery('md')`
+//       justify-content: unset
+//     `}
+//   `}
+// `;
 
-const Chip = styled.li`
-  ${({ theme: { themeColors } }) => css`
-    border: 1px solid ${themeColors.body};
-    border-radius: 2px;
-    color: ${themeColors.body};
-    padding: 0.25rem;
-  `}
-`;
+// const Chip = styled.li`
+//   ${({ theme: { themeColors } }) => css`
+//     border: 1px solid ${themeColors.body};
+//     border-radius: 2px;
+//     color: ${themeColors.body};
+//     padding: 0.25rem;
+//   `}
+// `;
 
-const ChipText = styled.p``;
+// const ChipText = styled.p``;
 
 const Description = styled.p`
   ${({ theme: { mediaQuery, themeColors } }) => css`
@@ -61,24 +62,30 @@ const ImgContainer = styled.div`
   `}
 `;
 
+const InnerContentContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex: 1 0 auto;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: flex-start;
+  padding: 1rem;
+`;
+
 const OuterCardLink = styled(Link)`
-  ${({ theme: { mediaQuery } }) => css`
-    align-items: center;
-    display: flex;
-    flex: 1 0 auto;
-    flex-direction: column;
-    gap: 0.5rem;
-    justify-content: space-between;
-    padding: 1rem;
-    ${mediaQuery('md')`
-      align-items: unset
-    `}
-  `}
+  display: flex;
+  flex: 1 0 auto;
+  flex-direction: column;
 `;
 
 const Title = styled.h3`
-  ${({ theme: { themeColors } }) => css`
+  ${({ theme: { mediaQuery, themeColors } }) => css`
     color: ${themeColors.headings};
+    text-align: center;
+
+    ${mediaQuery('lg')`
+      
+    `}
   `}
 `;
 
@@ -100,23 +107,26 @@ const ProjectCard = ({
   title
 }: ProjectCardTypes) => {
   console.log('In index.tsx, this is codeLink: ', codeLink);
+  console.log('In index.tsx, this is techStack: ', techStack);
   return (
     <CardContainer>
       <OuterCardLink href={liveLink} target="_blank">
-        <Title>{title}</Title>
-        <Description>{description}</Description>
         {!!image && (
           <ImgContainer>
             <Image alt={title} src={image} fill={true} />
           </ImgContainer>
         )}
-        <ChipsContainer>
-          {techStack.map((technology) => (
-            <Chip key={`${title}-${technology}`}>
-              <ChipText>{technology}</ChipText>
-            </Chip>
-          ))}
-        </ChipsContainer>
+        <InnerContentContainer>
+          <Title>{title}</Title>
+          <Description>{description}</Description>
+          {/* <ChipsContainer>
+            {techStack.map((technology) => (
+              <Chip key={`${title}-${technology}`}>
+                <ChipText>{technology}</ChipText>
+              </Chip>
+            ))}
+          </ChipsContainer> */}
+        </InnerContentContainer>
       </OuterCardLink>
     </CardContainer>
   );
