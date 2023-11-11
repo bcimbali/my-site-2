b7e8c88305b542fec8348e0c777d0205924f8152
Brett Cimbalik <bcimbali@gmail.com>
2023-11-11 11:58:15 -0600

    feat(asset): update mobile hamburger&close icons

--- src/icons/tsx/mobileXIcon.tsx
+++ src/icons/tsx/mobileXIcon.tsx
@@ -1,8 +1,17 @@
 import type { SVGProps } from 'react';
 import { memo } from 'react';
 const SvgMobileXIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={32} height={16} fill="none" {...props}>
-    <path stroke="#fff" strokeWidth={2} d="m31.588 14.911-31-14M31.423.906l-30 14" />
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" {...props}>
+    <path
+      fill="#fff"
+      d="M23.9 24 .003 2.037V0L24 22.054V24h-.1Z"
+      stroke={props.color || '#ffffff'}
+    />
+    <path
+      fill="#fff"
+      d="M0 24v-2.038L23.867 0h.131v1.918L0 24Z"
+      stroke={props.color || '#ffffff'}
+    />
   </svg>
 );
 const Memo = memo(SvgMobileXIcon);
