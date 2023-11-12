612186ebddf415aaf838b306211cdece4289e43b
Brett Cimbalik <bcimbali@gmail.com>
2023-11-12 17:55:51 -0600

    feat(component): start styling ShippingContainers

--- src/components/ShippingContainer/index.tsx
+++ src/components/ShippingContainer/index.tsx
@@ -3,7 +3,16 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-const ShipContainer = styled.div`
+const BottomBorder = styled.div`
+  background-color: hotpink;
+  border-top: 2px solid #000000;
+  height: 10px;
+  bottom: 0;
+  position: absolute;
+  width: 100%;
+`;
+
+const InnerShippingWrapper = styled.div`
   ${({ theme }) => css`
     align-items: center;
     border: 1px solid #000000;
@@ -24,18 +33,84 @@ const ShipContainer = styled.div`
   `}
 `;
 
+const LeftBorder = styled.div`
+  background-color: hotpink;
+  border-right: 2px solid #000000;
+  height: 100%;
+  left: 0;
+  position: absolute;
+  width: 30px;
+`;
+
+const MainText = styled.h2`
+  color: #ffffff;
+  font-size: 10rem;
+  margin: 0;
+`;
+
 const OuterShippingWrapper = styled.div`
-  ${({ theme }) => css`
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-  `}
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const RightBorder = styled.div`
+  background-color: hotpink;
+  border-left: 2px solid #000000;
+  height: 100%;
+  right: 0;
+  position: absolute;
+  width: 30px;
+`;
+
+const SmallVerticalText = styled.div`
+  color: #ffffff;
+  font-family: monospace;
+  font-size: 0.5rem;
+  margin: 0;
+  position: absolute;
+  top: 50px;
+  right: 80px;
+  text-orientation: upright;
+  writing-mode: vertical-rl;
+`;
+
+const SubTitle = styled.p`
+  color: #ffffff;
+  font-size: 3rem;
+  font-family: monospace;
+  margin: 0;
+`;
+
+const TitleContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TopBorder = styled.div`
+  background-color: hotpink;
+  border-bottom: 2px solid #000000;
+  height: 10px;
+  top: 0;
+  position: absolute;
+  width: 100%;
 `;
 
 const ShippingContainer = () => {
   return (
     <OuterShippingWrapper>
-      <ShipContainer />
+      <InnerShippingWrapper>
+        <TopBorder />
+        <LeftBorder />
+        <TitleContainer>
+          <MainText>ONE</MainText>
+          <SubTitle>OCEAN NETWORK EXPRESS</SubTitle>
+        </TitleContainer>
+        <RightBorder />
+        <BottomBorder />
+        <SmallVerticalText>H3T-L MRT</SmallVerticalText>
+      </InnerShippingWrapper>
     </OuterShippingWrapper>
   );
 };
