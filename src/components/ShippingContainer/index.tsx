e7cdfd6d1db6e8bb31a25c3bf53d9649a28d6667
Brett Cimbalik <bcimbali@gmail.com>
2023-11-14 08:27:04 -0600

    feat(page): update mobile shippingContainer sizes

--- src/components/ShippingContainer/index.tsx
+++ src/components/ShippingContainer/index.tsx
@@ -4,23 +4,29 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 const BottomBorder = styled.div`
-  background-color: hotpink;
-  border-top: 2px solid #000000;
-  height: 10px;
-  bottom: 0;
-  position: absolute;
-  width: 100%;
+  ${({ theme: { mediaQuery } }) => css`
+    background-color: hotpink;
+    border-top: 2px solid #000000;
+    height: 5px;
+    bottom: 0;
+    position: absolute;
+    width: 100%;
+
+    ${mediaQuery('lg')(`
+      height: 10px;
+    `)}
+  `}
 `;
 
 const InnerShippingWrapper = styled.div`
-  ${() => css`
+  ${({ theme: { mediaQuery } }) => css`
     align-items: center;
     border: 1px solid #000000;
     display: flex;
     justify-content: center;
     position: relative;
-    height: 400px;
-    width: 900px;
+    height: 200px;
+    width: 450px;
 
     background: repeating-linear-gradient(
         90deg,
@@ -30,22 +36,39 @@ const InnerShippingWrapper = styled.div`
         rgba(0, 0, 0, 0.3) 40px
       ),
       hotpink;
+
+    ${mediaQuery('lg')(`
+        height: 400px;
+        width: 900px;
+      `)}
   `}
 `;
 
 const LeftBorder = styled.div`
-  background-color: hotpink;
-  border-right: 2px solid #000000;
-  height: 100%;
-  left: 0;
-  position: absolute;
-  width: 30px;
+  ${({ theme: { mediaQuery } }) => css`
+    background-color: hotpink;
+    border-right: 2px solid #000000;
+    height: 100%;
+    left: 0;
+    position: absolute;
+    width: 15px;
+
+    ${mediaQuery('lg')(`
+      width: 30px;
+    `)}
+  `}
 `;
 
 const MainText = styled.h2`
-  color: #ffffff;
-  font-size: 10rem;
-  margin: 0;
+  ${({ theme: { mediaQuery } }) => css`
+    color: #ffffff;
+    font-size: 4rem;
+    margin: 0;
+
+    ${mediaQuery('lg')(`
+      font-size: 10rem;
+    `)}
+  `}
 `;
 
 const OuterShippingWrapper = styled.div`
@@ -55,31 +78,49 @@ const OuterShippingWrapper = styled.div`
 `;
 
 const RightBorder = styled.div`
-  background-color: hotpink;
-  border-left: 2px solid #000000;
-  height: 100%;
-  right: 0;
-  position: absolute;
-  width: 30px;
+  ${({ theme: { mediaQuery } }) => css`
+    background-color: hotpink;
+    border-left: 2px solid #000000;
+    height: 100%;
+    right: 0;
+    position: absolute;
+    width: 15px;
+
+    ${mediaQuery('lg')(`
+      width: 30px;
+    `)}
+  `}
 `;
 
 const SmallVerticalText = styled.div`
-  color: #ffffff;
-  font-family: monospace;
-  font-size: 0.5rem;
-  margin: 0;
-  position: absolute;
-  top: 50px;
-  right: 80px;
-  text-orientation: upright;
-  writing-mode: vertical-rl;
+  ${({ theme: { mediaQuery } }) => css`
+    color: #ffffff;
+    font-family: monospace;
+    font-size: 0.5rem;
+    margin: 0;
+    position: absolute;
+    top: 25px;
+    right: 26px;
+    text-orientation: upright;
+    writing-mode: vertical-rl;
+
+    ${mediaQuery('lg')(`
+      top: 50px;
+      right: 80px;
+    `)}
+  `}
 `;
 
 const SubTitle = styled.p`
-  color: #ffffff;
-  font-size: 3rem;
-  font-family: monospace;
-  margin: 0;
+  ${({ theme: { mediaQuery } }) => css`
+    color: #ffffff;
+    font-size: 1.5rem;
+    margin: 0;
+
+    ${mediaQuery('lg')(`
+      font-size: 3rem;
+    `)}
+  `}
 `;
 
 const TitleContainer = styled.div`
@@ -89,12 +130,18 @@ const TitleContainer = styled.div`
 `;
 
 const TopBorder = styled.div`
-  background-color: hotpink;
-  border-bottom: 2px solid #000000;
-  height: 10px;
-  top: 0;
-  position: absolute;
-  width: 100%;
+  ${({ theme: { mediaQuery } }) => css`
+    background-color: hotpink;
+    border-bottom: 2px solid #000000;
+    height: 5px;
+    top: 0;
+    position: absolute;
+    width: 100%;
+
+    ${mediaQuery('lg')(`
+      height: 10px;
+    `)}
+  `}
 `;
 
 const ShippingContainer = () => {
