8a768a0c6223bd882e14313e320237daeaedd897
Brett Cimbalik <bcimbali@gmail.com>
2023-11-15 20:12:59 -0600

    feat(page): add containersData for HomePage

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 import React from 'react';
 import PageTitle from '@/components/PageTitle';
 import TrainMarquee from '@/components/TrainMarquee';
-import ShippingContainer from '@/components/ShippingContainer';
-import Flatcar from 'src/components/Flatcar';
+import IntermodalWrapper from '@/components/IntermodalWrapper';
+import containersData from '@/lib/containersData';
 
 const StyledDiv = styled.div`
   display: flex;
@@ -15,45 +15,14 @@ const StyledDiv = styled.div`
   justify-content: space-between;
 `;
 
-const Intermodal = styled.div`
-  display: grid;
-`;
-
 const HomePage = () => {
   return (
     <StyledDiv>
       <PageTitle>Welcome</PageTitle>
       <TrainMarquee>
-        <Intermodal>
-          <ShippingContainer />
-          <ShippingContainer />
-          <Flatcar />
-        </Intermodal>
-        <ShippingContainer bgColor="#6050DC" title="EMP" subtitle="Eastern MI Parcel" />
-        <ShippingContainer
-          bgColor="#66FF00"
-          textColor="#1B4D3E"
-          title="SW"
-          subtitle="Southwestern"
-        />
-        <ShippingContainer
-          bgColor="#FFFF00"
-          textColor="#111111"
-          title="ZRE"
-          subtitle="Zeus Intermodal"
-        />
-        <ShippingContainer
-          bgColor="#A52A2A"
-          textColor="#CCCCFF"
-          title="LR-NW"
-          subtitle="Lisle Northwestern"
-        />
-        <ShippingContainer
-          bgColor="#F4F0EC"
-          textColor="#111111"
-          title="VLI"
-          subtitle="VLI  Network"
-        />
+        {containersData.map((box, idx) => (
+          <IntermodalWrapper key={`${box?.id}-${idx}-intermodal`} containers={box?.containers} />
+        ))}
       </TrainMarquee>
     </StyledDiv>
   );
