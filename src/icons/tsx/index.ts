f64abc0f1bb322715e3636f0d0968880d714adc2
Brett Cimbalik <bcimbali@gmail.com>
2023-11-27 09:15:17 -0600

    feat(component): add Onyx logo & position

--- src/icons/tsx/index.ts
+++ src/icons/tsx/index.ts
@@ -1,3 +1,4 @@
 export { default as Hamburger } from './hamburger';
 export { default as MobileXIcon } from './mobileXIcon';
+export { default as Onyx } from './onyx';
 export { default as Vmr } from './vmr';
