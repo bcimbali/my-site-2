e5e2430fc846163ffbc7abb19330daa25a333f0b
Brett Cimbalik <bcimbali@gmail.com>
2023-12-04 19:39:49 -0600

    feat(asset): add MKD logo & use in shipContainer

--- src/icons/tsx/index.ts
+++ src/icons/tsx/index.ts
@@ -1,5 +1,6 @@
 export { default as East } from './east';
 export { default as Hamburger } from './hamburger';
+export { default as Mkd } from './mkd';
 export { default as MobileXIcon } from './mobileXIcon';
 export { default as Onyx } from './onyx';
 export { default as StarBox } from './starBox';
--- src/lib/containersData.ts
+++ src/lib/containersData.ts
@@ -1,5 +1,6 @@
-import { East, Onyx, StarBox, Vmr } from '@/icons/tsx';
+import { East, Mkd, Onyx, StarBox, Vmr } from '@/icons/tsx';
 import { css } from 'styled-components';
+import { shippingContainer } from '@/styling/layout';
 
 const containersData = [
   {
@@ -205,9 +206,38 @@ const containersData = [
     containers: [
       {
         bgColor: '#F4F0EC',
+        containerCss: css`
+          ${({ theme: { mediaQuery, typography } }) => css`
+            .container-no {
+              bottom: 15px;
+              font-size: ${typography.mobile[0]};
+              font-weight: bold;
+              left: 35px;
+              right: unset;
+              text-orientation: unset;
+              top: unset;
+              writing-mode: unset;
+            }
+            ${mediaQuery('lg')(`
+            .container-no {
+              bottom: 30px;
+              font-size: ${typography.desktop[-1]};
+              left: 50px;
+              right: unset;
+              top: unset;
+            }
+        `)}
+          `}
+        `,
         containerNo: 'I7T-S ACY',
-        textColor: '#111111',
-        title: 'V   M'
+        logo: {
+          color: '#800000',
+          desktopWidth: shippingContainer.desktop.width,
+          fill: 'none',
+          mobileWidth: shippingContainer.mobile.width,
+          src: Mkd
+        },
+        textColor: '#800000'
       }
     ]
   }
