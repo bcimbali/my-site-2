1e5737443cf9f5836c3330d175e5f45a96c2def5
Brett Cimbalik <bcimbali@gmail.com>
2023-12-07 09:23:24 -0600

    feat(component): add backBttn to PageTitle compont

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -1,10 +1,33 @@
 'use client';
 
 import styled, { css } from 'styled-components';
+import Link from 'next/link';
 
 type TitleTypes = {
+  $hasBackButton?: boolean;
   $noMargin?: boolean;
 };
+
+const BackButton = styled(Link)`
+  ${({ theme: { mediaQuery, typography } }) => css`
+    font-size: ${typography.mobile[0]};
+    left: 0;
+    position: absolute;
+    top: -0.75rem;
+
+    ${mediaQuery('md')(`
+      top: -1.125rem;
+    `)}
+
+    ${mediaQuery('lg')(`
+      font-size: ${typography.desktop[0]};
+    `)}
+
+    ${mediaQuery('xxl')(`
+      top: -1.5rem;
+    `)}
+  `}
+`;
 
 const Title = styled.h1<TitleTypes>`
   ${({ $noMargin, theme: { mediaQuery, spacing, themeColors } }) => css`
@@ -18,12 +41,25 @@ const Title = styled.h1<TitleTypes>`
   `}
 `;
 
+const TitleWrapper = styled.div`
+  position: relative;
+`;
+
 const PageTitle = ({
   children,
-  noMargin = false
+  noMargin = false,
+  backButtonLink
 }: {
   children: React.ReactNode;
   noMargin?: boolean;
-}) => <Title $noMargin={noMargin}>{children}</Title>;
+  backButtonLink?: string;
+}) => (
+  <TitleWrapper>
+    <Title $noMargin={noMargin} $hasBackButton={!!backButtonLink}>
+      {children}
+    </Title>
+    {!!backButtonLink && <BackButton href={backButtonLink}>{`< Back`}</BackButton>}
+  </TitleWrapper>
+);
 
 export default PageTitle;
