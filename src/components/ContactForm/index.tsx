d5e54b3191ece98c6be712526bc124c09c5364d6
Brett Cimbalik <bcimbali@gmail.com>
2023-12-12 20:47:48 -0600

    feat(component): improve contact form markup

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -35,38 +35,46 @@ const OuterContainer = styled.div`
   width: 100%;
 `;
 
+const SuccessText = styled.h3`
+  text-align: center;
+`;
+
 const ContactForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors, isSubmitSuccessful, isSubmitting }
   } = useForm<Inputs>();
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
     <OuterContainer>
-      <h3>Contact Form</h3>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <InputWrapper>
-          <Label htmlFor="subject">Subject</Label>
-          <input defaultValue="" {...register('subject')} />
-        </InputWrapper>
+      {isSubmitSuccessful ? (
+        <SuccessText>Thanks for reaching out!</SuccessText>
+      ) : (
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <InputWrapper>
+            <Label htmlFor="subject">Subject</Label>
+            <input defaultValue="" {...register('subject', { required: true })} />
+            {errors.subject && <Error>This field is required</Error>}
+          </InputWrapper>
 
-        <InputWrapper>
-          <Label htmlFor="email">Email</Label>
-          <input defaultValue="" {...register('email', { required: true })} />
-          {errors.email && <Error>This field is required</Error>}
-        </InputWrapper>
+          <InputWrapper>
+            <Label htmlFor="email">Email</Label>
+            <input defaultValue="" {...register('email', { required: true })} />
+            {errors.email && <Error>This field is required</Error>}
+          </InputWrapper>
 
-        <InputWrapper>
-          <Label htmlFor="message">Message</Label>
-          <input defaultValue="" {...register('message', { required: true })} />
-          {errors.message && <Error>This field is required</Error>}
-        </InputWrapper>
+          <InputWrapper>
+            <Label htmlFor="message">Message</Label>
+            <textarea defaultValue="" {...register('message', { required: true })} />
+            {errors.message && <Error>This field is required</Error>}
+          </InputWrapper>
 
-        <input type="submit" />
-      </Form>
+          <input type="submit" disabled={isSubmitting} />
+        </Form>
+      )}
     </OuterContainer>
   );
 };
