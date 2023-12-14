d4a248a4c9d227aa0643d38a3d5673d3ead20bdb
Brett Cimbalik <bcimbali@gmail.com>
2023-12-13 20:39:54 -0600

    feat(page): add placeholder post req in contact

--- src/lib/contactFormSubmit.ts
+++ src/lib/contactFormSubmit.ts
@@ -3,8 +3,18 @@
 import { SubmitHandler } from 'react-hook-form';
 import { ContactFormFields } from '@/components/ContactForm';
 
-const contactFormSubmit: SubmitHandler<ContactFormFields> = (data) => {
+const contactFormSubmit: SubmitHandler<ContactFormFields> = async (data) => {
   console.log('in onSubmit, data is: ', data);
+
+  await fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 };
 
 export default contactFormSubmit;
