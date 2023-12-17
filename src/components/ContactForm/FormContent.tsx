8d645c5cbca799ca920a0ab66f7368ab7677bb5e
Brett Cimbalik <bcimbali@gmail.com>
2023-12-17 17:11:23 -0600

    feat(field): update TextArea w/ inputWrapper

--- src/components/ContactForm/FormContent.tsx
+++ src/components/ContactForm/FormContent.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { FieldErrors, UseFormRegister } from 'react-hook-form';
 import styled, { css } from 'styled-components';
 import { useFormStatus } from 'react-dom';
-import { ErrorMessage } from '@hookform/error-message';
 import TextInput from '@/fields/TextInput';
 import TextAreaInput from '@/fields/TextArea';
 
@@ -13,22 +12,6 @@ export interface FormValues {
   email: string;
   message: string;
 }
-
-const ErrorComponent = styled.span`
-  ${({ theme: { themeColors } }) => css`
-    color: ${themeColors.warning};
-  `}
-`;
-
-const InputWrapper = styled.div`
-  display: grid;
-`;
-
-const Label = styled.label`
-  ${({ theme: { colors } }) => css`
-    color: ${colors.white};
-  `}
-`;
 
 const LoadingText = styled.div`
   ${({ theme: { colors } }) => css`
@@ -61,11 +44,11 @@ const FormContent = ({
         label="Email"
       />
 
-      <InputWrapper>
-        <Label htmlFor="message">Message</Label>
-        <TextAreaInput input={{ ...register('message', { required: true }) }} />
-        <ErrorMessage name="message" errors={errors} as={ErrorComponent} />
-      </InputWrapper>
+      <TextAreaInput
+        input={{ ...register('message', { required: true }) }}
+        errors={errors}
+        label="Message"
+      />
 
       <input type="submit" disabled={pending || !isValid} />
       {pending && <LoadingText>Loading...</LoadingText>}
