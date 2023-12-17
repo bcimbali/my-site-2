7e35bd392db6ac6b8afe263ec12885647adc2d66
Brett Cimbalik <bcimbali@gmail.com>
2023-12-17 17:11:23 -0600

    feat(field): update TextArea w/ inputWrapper

--- src/fields/TextArea/index.tsx
+++ src/fields/TextArea/index.tsx
@@ -2,24 +2,43 @@
 
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { ChangeHandler, RefCallBack } from 'react-hook-form';
+import { ChangeHandler, FieldErrors, RefCallBack } from 'react-hook-form';
+import { FormValues } from '@/components/ContactForm';
+import { ErrorMessage } from '@hookform/error-message';
 
 type InputTypes = {
+  errors?: FieldErrors<FormValues>;
   input: {
-    name: string;
+    name: keyof FormValues;
     onBlur: ChangeHandler;
     onChange: ChangeHandler;
     ref: RefCallBack;
   };
+  label?: string;
 };
 
-const Input = styled.textarea`
-  ${({ theme: { colors, mediaQuery, typography } }) => css`
+type InputComponentTypes = {
+  $hasErrors?: boolean;
+};
+
+const ErrorComponent = styled.span`
+  ${({ theme: { themeColors } }) => css`
+    color: ${themeColors.warning};
+  `}
+`;
+
+const Input = styled.textarea<InputComponentTypes>`
+  ${({ $hasErrors, theme: { colors, mediaQuery, themeColors, typography } }) => css`
     background: ${colors.blue};
     border: 1px solid ${colors.white};
     border-radius: 4px;
     color: ${colors.white};
     padding: ${typography.mobile[-3]};
+
+    ${$hasErrors &&
+    css`
+      border-color: ${themeColors?.danger};
+    `}
 
     ${mediaQuery('lg')(`
       padding: ${typography.desktop[-3]};
@@ -27,9 +46,25 @@ const Input = styled.textarea`
   `}
 `;
 
-const TextAreaInput = ({ input }: InputTypes) => {
-  console.log('In index.tsx, this is input: ', input);
-  return <Input {...input} />;
+const InputWrapper = styled.div`
+  display: grid;
+`;
+
+const Label = styled.label`
+  ${({ theme: { colors } }) => css`
+    color: ${colors.white};
+  `}
+`;
+
+const TextAreaInput = ({ errors, input, label }: InputTypes) => {
+  const hasErrors = !!errors?.[`${input?.name}`];
+  return (
+    <InputWrapper>
+      <Label htmlFor={input?.name}>{label}</Label>
+      <Input {...input} $hasErrors={hasErrors} />
+      {errors && <ErrorMessage name={input.name} errors={errors} as={ErrorComponent} />}
+    </InputWrapper>
+  );
 };
 
 export default TextAreaInput;
