2690179357992e2bd092c073341899dcf955a8ff
Brett Cimbalik <bcimbali@gmail.com>
2023-12-18 19:41:49 -0600

    feat(component): move east to own ship_container

--- src/shipping_containers/index.ts
+++ src/shipping_containers/index.ts
@@ -1,8 +1,9 @@
+export { default as caretContainer } from './caret';
+export { default as eastContainer } from './east';
+export { default as ftLogisticsContainer } from './ftLogistics';
 export { default as onyxContainer } from './onyx';
-export { default as wishContainer } from './wish';
+export { default as starboxContainer } from './starbox';
+export { default as sunshineContainer } from './sunshine';
 export { default as vmrOneContainer } from './vmrOne';
 export { default as vmrTwoContainer } from './vmrTwo';
-export { default as sunshineContainer } from './sunshine';
-export { default as caretContainer } from './caret';
-export { default as starboxContainer } from './starbox';
-export { default as ftLogisticsContainer } from './ftLogistics';
+export { default as wishContainer } from './wish';
