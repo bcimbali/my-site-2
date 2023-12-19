f3e1666ce68cd265533c5547a638382fe1ed7082
Brett Cimbalik <bcimbali@gmail.com>
2023-12-18 19:38:59 -0600

    feat(component): move ftLogistics to ship_containr

---
+++ src/shipping_containers/ftLogistics.ts
@@ -0,0 +1,17 @@
+import { FtLogistics } from '@/icons/tsx';
+import { shippingContainer } from '@/styling/layout';
+
+const ftLogistics = {
+  bgColor: '#66FF00',
+  containerNo: 'C8W-I ZEZ',
+  logo: {
+    color: '#1B4D3E',
+    desktopWidth: shippingContainer.desktop.width,
+    fill: '#1B4D3E',
+    mobileWidth: shippingContainer.mobile.width,
+    src: FtLogistics
+  },
+  textColor: '#1B4D3E'
+};
+
+export default ftLogistics;
---
+++ src/shipping_containers/vmrTwo.ts
@@ -0,0 +1,16 @@
+import { Vmr } from '@/icons/tsx';
+
+const vmrTwo = {
+  bgColor: '#1B4D3E',
+  containerNo: 'E9A-A KJG',
+  logo: {
+    color: 'none',
+    desktopWidth: '130px',
+    fill: '#AAAAAA',
+    mobileWidth: '100px',
+    src: Vmr
+  },
+  textColor: '#AAAAAA'
+};
+
+export default vmrTwo;
