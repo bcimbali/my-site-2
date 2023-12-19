0868563bcb1788f3047f29255d68b0ab5f14aa65
Brett Cimbalik <bcimbali@gmail.com>
2023-12-18 19:54:13 -0600

    feat(component): move mkd to shipping_containers

--- src/lib/containersData.ts
+++ src/lib/containersData.ts
@@ -1,11 +1,9 @@
-import { Mkd } from '@/icons/tsx';
-import { css } from 'styled-components';
-import { shippingContainer } from '@/styling/layout';
 import {
   caretContainer,
   doveContainer,
   eastContainer,
   ftLogisticsContainer,
+  mkdContainer,
   onyxContainer,
   starboxContainer,
   sunshineContainer,
@@ -53,43 +51,7 @@ const containersData = [
   },
   {
     id: 'DTTX-4132',
-    containers: [
-      {
-        bgColor: '#F4F0EC',
-        containerCss: css`
-          ${({ theme: { mediaQuery, typography } }) => css`
-            .container-no {
-              bottom: 15px;
-              font-size: ${typography.mobile[-1]};
-              font-weight: bold;
-              left: 35px;
-              right: unset;
-              text-orientation: unset;
-              top: unset;
-              writing-mode: unset;
-            }
-            ${mediaQuery('lg')(`
-            .container-no {
-              bottom: 30px;
-              font-size: ${typography.desktop[-1]};
-              left: 50px;
-              right: unset;
-              top: unset;
-            }
-        `)}
-          `}
-        `,
-        containerNo: 'I7T-S ACY',
-        logo: {
-          color: '#800000',
-          desktopWidth: shippingContainer.desktop.width,
-          fill: 'none',
-          mobileWidth: shippingContainer.mobile.width,
-          src: Mkd
-        },
-        textColor: '#800000'
-      }
-    ]
+    containers: [mkdContainer]
   }
 ];
 
