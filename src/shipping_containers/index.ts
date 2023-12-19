594aaf48476d148e12f05e90e7abf20d6414ab3e
Brett Cimbalik <bcimbali@gmail.com>
2023-12-18 19:27:07 -0600

    feat(component): move more to shipping_containers

--- src/shipping_containers/index.ts
+++ src/shipping_containers/index.ts
@@ -1,2 +1,4 @@
 export { default as onyxContainer } from './onyx';
 export { default as wishContainer } from './wish';
+export { default as vmrOneContainer } from './vmrOne';
+export { default as sunshineContainer } from './sunshine';
---
+++ src/shipping_containers/vmrOne.ts
@@ -0,0 +1,16 @@
+import { Vmr } from '@/icons/tsx';
+
+const vmrOne = {
+  bgColor: '#BADA55',
+  containerNo: 'R2G-E DUP',
+  logo: {
+    color: 'none',
+    desktopWidth: '130px',
+    fill: '#002244',
+    mobileWidth: '100px',
+    src: Vmr
+  },
+  textColor: '#002244'
+};
+
+export default vmrOne;
