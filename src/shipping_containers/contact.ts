54d5d521c42bb6bf56eac9a664a395196e57a6ca
Brett Cimbalik <bcimbali@gmail.com>
2023-12-28 12:11:40 -0500

    feat(component): add initial contact container

---
+++ src/shipping_containers/contact.ts
@@ -0,0 +1,15 @@
+import { About } from '@/icons/tsx';
+
+const contact = {
+  bgColor: '#ADD8E6',
+  containerNo: 'AB-O1T EKL',
+  link: '/contact',
+  logo: {
+    color: '#191970',
+    fill: '#191970',
+    src: About
+  },
+  textColor: '#191970'
+};
+
+export default contact;
