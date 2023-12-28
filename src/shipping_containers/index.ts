868b52045b7aaf7b153833c17a6bd716d39191de
Brett Cimbalik <bcimbali@gmail.com>
2023-12-28 10:51:55 -0500

    feat(component): add initial spNextSiteContainer

--- src/shipping_containers/index.ts
+++ src/shipping_containers/index.ts
@@ -6,6 +6,7 @@ export { default as eastContainer } from './east';
 export { default as ftLogisticsContainer } from './ftLogistics';
 export { default as mkdContainer } from './mkd';
 export { default as onyxContainer } from './onyx';
+export { default as spNextSiteContainer } from './spNextSite';
 export { default as starboxContainer } from './starbox';
 export { default as sunshineContainer } from './sunshine';
 export { default as vmrOneContainer } from './vmrOne';
