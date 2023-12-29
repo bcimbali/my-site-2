ddef97780b1177a79d8267ec38122e74544a7ec8
Brett Cimbalik <bcimbali@gmail.com>
2023-12-29 16:50:37 -0600

    fix(cleanup):" remove unused BrunoAceSC font

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,4 +1,3 @@
-import { Bruno_Ace_SC, Play } from 'next/font/google';
+import { Play } from 'next/font/google';
 
 export const play = Play({ display: 'swap', weight: ['400', '700'], subsets: ['latin'] });
-export const brunoAceSc = Bruno_Ace_SC({ display: 'swap', weight: ['400'], subsets: ['latin'] });
