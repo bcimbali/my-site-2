d956f12992deaa41ee19e44610e841088b4b8931
Brett Cimbalik <bcimbali@gmail.com>
2023-12-29 16:42:31 -0600

    feat(component): add genGenericContainer util fn

--- src/lib/homepageTrainData.ts
+++ src/lib/homepageTrainData.ts
@@ -3,19 +3,27 @@ import {
   bc010Container,
   ckSiteContainer,
   contactContainer,
-  genericOneContainer,
   projectsContainer,
   spNextSiteContainer
 } from '@/shipping_containers';
+import genGenericContainer from '@/lib/genGenericContainer';
+
+// Create same but different non-link shipping containers:
+const unclickableOne = genGenericContainer({ containerNo: 'LRT-P CCF' });
+const unclickableTwo = genGenericContainer({ containerNo: 'XIR-8 WWT' });
+const unclickableThree = genGenericContainer({ containerNo: 'MNT-P ACI' });
+const unclickableFour = genGenericContainer({ containerNo: 'ARC-T EMV' });
+const unclickableFive = genGenericContainer({ containerNo: 'OST-F REB' });
+const unclickableSix = genGenericContainer({ containerNo: 'SSR-C JES' });
 
 const homepageTrainData = [
   {
     id: 'RVVX-1197',
-    containers: [bc010Container, genericOneContainer]
+    containers: [bc010Container, unclickableOne]
   },
   {
     id: 'RVVX-7201',
-    containers: [genericOneContainer, contactContainer],
+    containers: [unclickableSix, contactContainer],
     flatcarColor: '#7b3c3c'
   },
   {
@@ -24,12 +32,12 @@ const homepageTrainData = [
   },
   {
     id: 'RVVX-7789',
-    containers: [genericOneContainer],
+    containers: [unclickableTwo],
     flatcarColor: '#4B5320'
   },
   {
     id: 'RVVX-9209',
-    containers: [ckSiteContainer, genericOneContainer]
+    containers: [ckSiteContainer, unclickableThree]
   },
   {
     id: 'RVVX-5142',
@@ -37,7 +45,7 @@ const homepageTrainData = [
   },
   {
     id: 'RVVX-0702',
-    containers: [genericOneContainer, projectsContainer]
+    containers: [unclickableFour, projectsContainer]
   },
   {
     id: 'RVVX-9765',
@@ -45,7 +53,7 @@ const homepageTrainData = [
   },
   {
     id: 'RVVX-8244',
-    containers: [aboutContainer, genericOneContainer],
+    containers: [aboutContainer, unclickableFive],
     flatcarColor: '#71797E'
   },
   {
