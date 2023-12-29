86da67d0dacf7356d384e8801fd75c35ad438ae7
Brett Cimbalik <bcimbali@gmail.com>
2023-12-29 16:18:02 -0600

    feat(page): tweak contrastColors for consistency

--- src/shipping_containers/ckSite.ts
+++ src/shipping_containers/ckSite.ts
@@ -1,16 +1,26 @@
 import { CkSite } from '@/icons/tsx';
-import theme from '@/styling/themes/darkTheme';
+import { css } from 'styled-components';
+
+const bgColor = 'rgb(177, 176, 229)';
+const contrastColor = '#242953';
 
 const ckSite = {
-  bgColor: 'rgb(177, 176, 229)',
+  bgColor,
+  containerCss: css`
+    .container-no {
+      border-bottom: 1px solid ${contrastColor};
+      border-top: 1px solid ${contrastColor};
+      padding: 4px 0;
+    }
+  `,
   containerNo: 'CK-32 LPQ',
   link: '/projects/ck-site',
   logo: {
-    color: theme.colors.white,
-    fill: theme.colors.white,
+    color: contrastColor,
+    fill: contrastColor,
     src: CkSite
   },
-  textColor: theme.colors.white
+  textColor: contrastColor
 };
 
 export default ckSite;
