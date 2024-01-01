034da855c51be38fde4127d591a7be3736b99e82
Brett Cimbalik <bcimbali@gmail.com>
2024-01-01 15:38:47 -0600

    fix(ally): set tabIndex -1 for hidden containers

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -27,6 +27,25 @@ const TitleContainer = styled.div`
   flex-direction: column;
 `;
 
+const trainMarkup = homepageTrainData.map((item, idx) => (
+  <IntermodalWrapper
+    key={`${item?.id}-${idx}-intermodal`}
+    containers={item?.containers}
+    flatcarColor={item?.flatcarColor}
+    id={item.id}
+  />
+));
+
+const hiddenTrainMarkup = homepageTrainData.map((item, idx) => (
+  <IntermodalWrapper
+    key={`${item?.id}-${idx}-intermodal`}
+    containers={item?.containers}
+    flatcarColor={item?.flatcarColor}
+    id={item.id}
+    isHidden={true}
+  />
+));
+
 const HomePage = () => {
   const pathname = usePathname();
   const { setTrainState } = useContext(TrainContext);
@@ -40,16 +59,7 @@ const HomePage = () => {
         <PageTitle noMargin>{'Brett Cimbalik'}</PageTitle>
         <PageSubtitle>Web Developer</PageSubtitle>
       </TitleContainer>
-      <TrainMarquee>
-        {homepageTrainData.map((item, idx) => (
-          <IntermodalWrapper
-            key={`${item?.id}-${idx}-intermodal`}
-            containers={item?.containers}
-            flatcarColor={item?.flatcarColor}
-            id={item.id}
-          />
-        ))}
-      </TrainMarquee>
+      <TrainMarquee hiddenTrainMarkup={hiddenTrainMarkup}>{trainMarkup}</TrainMarquee>
 
       <CommsTower amountOfLights={4} height="300px" left="0" width="1px" />
       <CommsTower amountOfLights={6} height="500px" left="100%" width="1px" />
