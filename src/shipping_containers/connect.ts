4ff17cdeba933670e93bc97f7b9c2e97fe06f50b
Brett Cimbalik <bcimbali@gmail.com>
2024-01-03 20:15:01 -0600

    feat(component): add ConnectContainer

---
+++ src/shipping_containers/connect.ts
@@ -0,0 +1,19 @@
+import { Connect } from '@/icons/tsx';
+
+const bgColor = '#0077B5';
+const contrastColor = '#DDDDDD';
+
+const connect = {
+  bgColor,
+  containerNo: 'CN-02 NCT',
+  link: 'https://www.linkedin.com/in/brett-cimbalik/',
+  externalLink: true,
+  logo: {
+    color: contrastColor,
+    fill: contrastColor,
+    src: Connect
+  },
+  textColor: contrastColor
+};
+
+export default connect;
