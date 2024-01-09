f053be8cb7930f928deaea9db5f1fd2b6763adc6
Brett Cimbalik <bcimbali@gmail.com>
2024-01-08 21:10:44 -0600

    feat(seo): add metadataBase url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,18 @@ import ThemeProvider from '@/app/theme-provider';
 import PageContainer from '@/components/PageContainer';
 import TrainProvider from '@/app/train-provider';
 
+const urlBase =
+  process.env.VERCEL_ENV === 'preview'
+    ? `https://${process.env.VERCEL_URL}`
+    : process.env.VERCEL_ENV === 'production'
+      ? 'https://brettcimbalik.com'
+      : `http://localhost:3000`;
+
 export const metadata: Metadata = {
   title: 'Brett Cimbalik',
   description: 'Web development and software engineering portfolio for Brett Cimbalik.',
-  creator: 'Brett Cimbalik'
+  creator: 'Brett Cimbalik',
+  metadataBase: new URL(urlBase)
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
