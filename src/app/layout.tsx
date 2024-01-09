5fdc0a73e98f8cf2700bc18a50d84a5e049d9a04
Brett Cimbalik <bcimbali@gmail.com>
2024-01-08 21:31:10 -0600

    fix(seo): update prod canonical base url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const urlBase =
   process.env.VERCEL_ENV === 'preview'
     ? `https://${process.env.VERCEL_BRANCH_URL}`
     : process.env.VERCEL_ENV === 'production'
-      ? 'https://brettcimbalik.com'
+      ? 'https://www.brettcimbalik.com'
       : `http://localhost:3000`;
 
 export const metadata: Metadata = {
