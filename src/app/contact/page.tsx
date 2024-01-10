172a9f315095e25627fc54a7705a45bc618becaf
Brett Cimbalik <bcimbali@gmail.com>
2024-01-09 21:01:45 -0600

    feat(page): add og image to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,6 +7,14 @@ export const metadata = {
     languages: {
       'en-US': '/en-US'
     }
+  },
+  openGraph: {
+    title: 'Brett Cimbalik | Contact',
+    description: 'Brett Cimbalik contact section.',
+    url: '/contact',
+    siteName: 'Brett Cimbalik',
+    locale: 'en_US',
+    type: 'website'
   }
 };
 
