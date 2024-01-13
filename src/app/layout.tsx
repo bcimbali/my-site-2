87cb8fd02e139acc3f0e1f6a3124341e708b21ec
Brett Cimbalik <bcimbali@gmail.com>
2024-01-13 11:33:20 -0600

    feat(seo): add JSON-LD to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { WebSite, WithContext } from 'schema-dts';
 import './globals.css';
 import StyledComponentsRegistry from '@/lib/registry';
 import ThemeProvider from '@/app/theme-provider';
@@ -15,8 +16,22 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const jsonLd: WithContext<WebSite> = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    about: 'Web development and software engineering portfolio for Brett Cimbalik.',
+    creator: 'Brett Cimbalik',
+    image: '/favicon.ico',
+    description: 'Web development and software engineering portfolio for Brett Cimbalik.',
+    name: 'Brett Cimbalik'
+  };
+
   return (
     <html lang="en">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <StyledComponentsRegistry>
         <TrainProvider>
           <ThemeProvider>
