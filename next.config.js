10ab2d7613d9974496a8512cdf8d65ddff3f0b6a
Brett Cimbalik <bcimbali@gmail.com>
2024-01-13 16:12:26 -0600

    feat(config): add bundleAnalyzer to nextConfig

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
@@ -47,4 +52,4 @@ const nextConfig = {
   // ...rest of config
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
