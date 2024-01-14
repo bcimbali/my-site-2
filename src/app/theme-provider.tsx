b9d88de7143919ac9274bc8b1c682fd949965f46
Brett Cimbalik <bcimbali@gmail.com>
2024-01-14 13:50:05 -0600

    feat(asset): add sun & moon icons for ThemeSwitchr

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -5,10 +5,10 @@ import { ThemeProvider } from 'styled-components';
 import themes from '@/styling/themes';
 import GlobalStyles from '@/styling/globalStyles';
 import { ThemeStylesContext } from '@/context/themeStylesContext';
-// console.log('In theme-provider.tsx, this is theme: ', theme);
 
 export default function ThemeProviderComponent({ children }: { children: React.ReactNode }) {
   const { themeState } = useContext(ThemeStylesContext);
+  console.log('In theme-provider.tsx, this is theme: ', themes[themeState]);
   return (
     <ThemeProvider theme={themes[themeState]}>
       <GlobalStyles />
